f45a7d26be394e5165b2e09999250e324b6997ad
dtoddwalker002-gif <dtoddwalker002@gmail.com>
2025-09-24 00:32:44 -0500

    Completed Task 2 Output

---
+++ external.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+/*
+document.write("Where is my JS code?");
+*/
+/*
+window.alert(5+6);
+*/
+/*
+window.alert('5+6');
+*/
+document.write('<h1>A Heading</h1>');
+document.write('<p>A Sentence.</p>');
