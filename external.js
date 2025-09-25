3c73566160003ad47ce220c2b03f934cfc44c4a5
dtoddwalker002-gif <dtoddwalker002@gmail.com>
2025-09-25 15:33:51 -0500

    Completed task 4 operators

--- external.js
+++ external.js
@@ -19,7 +19,16 @@ const x ="web";
 const y ="mapping";
 const out = x + y;
 document.write(out);
-*/
-/ A prompt box is used to prompt users to input a value before entering a page.
+
+// A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
+*/
+
+a = 22;
+const b = 33;
+document.writeln(a + b);
+document.writeln("<br>");
+document.writeln(a += b);
+document.writeln("<br>");
+document.writeln(a += b);
