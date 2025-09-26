37baeb5c6b7df8a848a9389c60e2f75b3c367ab4
dtoddwalker002-gif <dtoddwalker002@gmail.com>
2025-09-25 19:48:06 -0500

    Automated Table Creation for webmpa reviews.

--- external.js
+++ external.js
@@ -6,7 +6,24 @@ let a = "Please enter your name.";
 let b = "Type your name here.";
 // A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt(a, b);
- message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+let message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
 return message
 }
 document.write(welcome());
+document.write(message);
+
+function webmap_table()
+{
+document.write("<table width=100%>");
+for (var row=0; row < 2; row++)
+{
+document.write("<tr>");
+for (var column=0; column < 3; column++)
+{
+document.write("<td>" + row + "," + column + "</td>");
+}
+document.write("</tr>");
+}
+document.write("</table>");
+return "";
+}
