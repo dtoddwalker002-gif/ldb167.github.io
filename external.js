d645348a02f8253fafa5c6eb277eb3a198391dab
dtoddwalker002-gif <dtoddwalker002@gmail.com>
2025-09-25 19:26:03 -0500

    Defined a function for welcome message

--- external.js
+++ external.js
@@ -1,16 +1,12 @@
 
 
-
-
-function hello(){
-
-var name = document.getElementById('name').value;
-
-var greeting = "hello," + name +"!";
-
-
-
-
-
-
+function welcome()
+{
+let a = "Please enter your name.";
+let b = "Type your name here.";
+// A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt(a, b);
+ message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+return message
 }
+document.write(welcome());
